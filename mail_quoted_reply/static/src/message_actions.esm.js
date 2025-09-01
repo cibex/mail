37236509cf2df08c0715e2e762248bb3cb33a6ce
Christoph Abenthung <christoph.abenthung@cibex.net>
2025-09-01 09:21:48 +0200

    [IMP]mail_quoted_reply: add reply to all

--- mail_quoted_reply/static/src/message_actions.esm.js
+++ mail_quoted_reply/static/src/message_actions.esm.js
@@ -6,3 +6,10 @@ messageActionsRegistry.add("reply", {
     onClick: (component) => component.message.messageReply(component.props.message),
     condition: (component) => component.canReply,
 });
+messageActionsRegistry.add("reply_all", {
+    icon: "fa fa-reply-all",
+    title: "Reply All",
+    onClick: (component) =>
+        component.message.messageReply(component.props.message, true),
+    condition: (component) => component.canReply,
+});
